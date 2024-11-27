7f610a6549c531545f8aa567ec68253da921d2c0
Raphael Ramos <raphael.opensource@gmail.com>
2024-11-26 22:40:49 -0300

    Include Profit and Tax column

--- lucro-criptos/main.py
+++ lucro-criptos/main.py
@@ -4,12 +4,9 @@ import numpy as np
 ## Config
 
 OP_IGNORED = ['Deposit', 'Transfer Between Main and Funding Wallet']
-
 EXCHANGE = 'BINANCE'
-
-COIN_TARGET = 'BNB'
+COIN_TARGET = 'BTC'
 COIN_BRL = 'BRL'
-
 TRADE_TYPE_BUY = 'c'
 TRADE_TYPE_SELL = 'v'
 
@@ -17,7 +14,7 @@ pd.set_option("display.precision", 8)
 
 ## Read settlements
 
-df = pd.read_csv('data/test-1.csv')
+df = pd.read_csv('data/2024-0111-2411.csv')
 df = df[~(df.Operation.isin(OP_IGNORED))]
 df = df[['Coin', 'Change']]
 
@@ -31,8 +28,12 @@ df_concat = target_df.join(brl_df, lsuffix='_TARGET', rsuffix='_BRL')
 
 df_concat['Price'] = df_concat.Change_BRL.abs()/df_concat.Change_TARGET.abs()
 df_concat['Quantity'] = df_concat.Change_TARGET.abs()
+df_concat['Investment'] = df_concat.Change_BRL
+df_concat['Profit'] = df_concat['Investment'].cumsum()
 df_concat['Type'] = df_concat.Change_BRL.apply(lambda change : TRADE_TYPE_SELL if change > 0 else TRADE_TYPE_BUY)
-df_trades = df_concat[['Price', 'Quantity', 'Type']].reset_index(drop=True)
+df_concat['Tax'] = df_concat.Profit.apply(lambda profit : 0.15*profit if profit > 0 else 0)
+
+df_trades = df_concat[['Price', 'Quantity', 'Investment', 'Type', 'Profit', 'Tax']].reset_index(drop=True)
 
 df_buy_trades = df_trades[df_trades.Type == TRADE_TYPE_BUY]
 total_buy = (df_buy_trades.Price * df_buy_trades.Quantity).sum()
@@ -51,14 +52,11 @@ amm_coins = (np.where(df_trades.Type == TRADE_TYPE_BUY, df_trades.Quantity, (-1)
 ### Sales balance
 sales_balance = total_sell - mean_price * df_sell_trades.Quantity.sum()
 
-### Discount Income Tax (IR)
-ir = sales_balance * 0.15 if sales_balance > 0 else 0.00
-
 ### Investment
 investment = mean_price * amm_coins
 
-print(f'Qtd Moedas: {amm_coins:.8}')
-print(f'Saldo de Vendas: R$ {sales_balance:.2f}')
-print(f'IR: R$ {ir:.2f}')
-print(f'Preço médio: R$ {mean_price:.2f}')
+print("Declaração IR:")
 print(f"Tenho {amm_coins:.8} {COIN_TARGET}, adquiridos a um custo médio de R$ {mean_price:,.2f}, totalizando um investimento de R$ {investment:,.2f}, custodiados na corretora {EXCHANGE}.")
+
+print(f"Quadro de aplicações:")
+print(df_trades)
